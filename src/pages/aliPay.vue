5f48b6dcfa18051213056be2b055544bf10adc76
kovlento <bluskytee@163.com>
2020-03-30 23:42:51 +0800

    pay

--- src/pages/aliPay.vue
+++ src/pages/aliPay.vue
@@ -1,9 +1,43 @@
 <template>
-  <div>alipay</div>
+  <div class="ali-pay">
+    <loading v-if="loading"></loading>
+    <div class="form" v-html="content"></div>
+  </div>
 </template>
 
 <script>
-export default {}
+import Loading from '../components/Loading'
+export default {
+  name: 'alipay',
+  components: { Loading },
+  data() {
+    return {
+      orderId: this.$route.query.orderNo,
+      content: '',
+      loading: true
+    }
+  },
+  mounted() {
+    this.paySubmit()
+  },
+  methods: {
+    paySubmit() {
+      this.axios
+        .post('/pay', {
+          orderId: this.orderId,
+          orderName: '手机订单',
+          amount: 0.01,
+          payType: 1
+        })
+        .then(res => {
+          this.content = res.content
+          setTimeout(() => {
+            document.forms[0].submit()
+          }, 200)
+        })
+    }
+  }
+}
 </script>
 
 <style></style>
